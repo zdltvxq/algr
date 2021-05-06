21abc29ac68999d8a00f866c992e5b80c185d485
zhoudongliang <zhoudongliang@allinmd.cn>
2021-05-06 17:54:20 +0800

    5

--- src/main/java/com/z/algr/test/Jzof.java
+++ src/main/java/com/z/algr/test/Jzof.java
@@ -29,9 +29,14 @@ public class Jzof {
         int pivot, left = 0, right = nums.length - 1;
         while (left <= right) {
             pivot = left + (right - left) / 2;
-            if (nums[pivot] == target) return pivot;
-            if (target < nums[pivot]) right = pivot - 1;
-            else left = pivot + 1;
+            if (nums[pivot] == target) {
+                return pivot;
+            }
+            if (target < nums[pivot]) {
+                right = pivot - 1;
+            } else {
+                left = pivot + 1;
+            }
         }
         return -1;
     }
